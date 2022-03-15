d003a2c99bb22cb341475726610f61f485f868b4
klawingco <marcjhon18@gmail.com>
2022-03-15 10:22:48 +0800

    discuss 1.13.3 elixir

--- discuss/config/config.exs
+++ discuss/config/config.exs
@@ -1,27 +1,52 @@
 # This file is responsible for configuring your application
-# and its dependencies with the aid of the Mix.Config module.
+# and its dependencies with the aid of the Config module.
 #
 # This configuration file is loaded before any dependency and
 # is restricted to this project.
-use Mix.Config
 
 # General application configuration
+import Config
+
 config :discuss,
   ecto_repos: [Discuss.Repo]
 
 # Configures the endpoint
-config :discuss, Discuss.Endpoint,
+config :discuss, DiscussWeb.Endpoint,
   url: [host: "localhost"],
-  secret_key_base: "T6bXJ7Qv52b0dVXllchNc0qdIZiV7q2ufPIYMT07F0pGI352BwGgvDk517FOx6iS",
-  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Discuss.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  render_errors: [view: DiscussWeb.ErrorView, accepts: ~w(html json), layout: false],
+  pubsub_server: Discuss.PubSub,
+  live_view: [signing_salt: "v+k/k5fe"]
+
+# Configures the mailer
+#
+# By default it uses the "Local" adapter which stores the emails
+# locally. You can see the emails in your browser, at "/dev/mailbox".
+#
+# For production it's recommended to configure a different adapter
+# at the `config/runtime.exs`.
+config :discuss, Discuss.Mailer, adapter: Swoosh.Adapters.Local
+
+# Swoosh API client is needed for adapters other than SMTP.
+config :swoosh, :api_client, false
+
+# Configure esbuild (the version is required)
+config :esbuild,
+  version: "0.14.0",
+  default: [
+    args:
+      ~w(js/app.js --bundle --target=es2017 --outdir=../priv/static/assets --external:/fonts/* --external:/images/*),
+    cd: Path.expand("../assets", __DIR__),
+    env: %{"NODE_PATH" => Path.expand("../deps", __DIR__)}
+  ]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
+# Use Jason for JSON parsing in Phoenix
+config :phoenix, :json_library, Jason
+
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{config_env()}.exs"
--- discuss/config/test.exs
+++ discuss/config/test.exs
@@ -1,19 +1,30 @@
-use Mix.Config
+import Config
+
+# Configure your database
+#
+# The MIX_TEST_PARTITION environment variable can be used
+# to provide built-in test partitioning in CI environment.
+# Run `mix help test` for more information.
+config :discuss, Discuss.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "discuss_test#{System.get_env("MIX_TEST_PARTITION")}",
+  pool: Ecto.Adapters.SQL.Sandbox,
+  pool_size: 10
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
-config :discuss, Discuss.Endpoint,
-  http: [port: 4001],
+config :discuss, DiscussWeb.Endpoint,
+  http: [ip: {127, 0, 0, 1}, port: 4002],
+  secret_key_base: "f6MCCvn6CcNL0fO2VHhmup3RY8wjvsp5zg51U5ki0fo6NWElYkDsTVGdkvdZMSIx",
   server: false
+
+# In test we don't send emails.
+config :discuss, Discuss.Mailer, adapter: Swoosh.Adapters.Test
 
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
-config :discuss, Discuss.Repo,
-  adapter: Ecto.Adapters.Postgres,
-  username: "postgres",
-  password: "postgres",
-  database: "discuss_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
+# Initialize plugs at runtime for faster test compilation
+config :phoenix, :plug_init_mode, :runtime
